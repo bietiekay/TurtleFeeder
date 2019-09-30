0d80dec82eebfcc1dd1d8096471d618b1f98ecbb
Daniel Kirstenpfad <bietiekay@gmail.com>
2019-09-30 13:23:20 +0200

    fixing mqtt reconnect restarts

--- src/TurtleFeeder.ino
+++ src/TurtleFeeder.ino
@@ -1,30 +1,30 @@
 /* Arduino code and information on how to modify a dobar or SaiDeng automatic fish pond feeder
  * (C) Daniel Kirstenpfad 2019
- *
+ * 
  * Requirements / Libraries:
  *  - Arduino IDE 1.8.8
  *  - Arduino ESP8266 library 2.5.2 - https://github.com/esp8266/Arduino
  *  - Arduino MQTT Library 2.4.1 by 256dpi - https://github.com/256dpi/arduino-mqtt
- *
+ * 
  * This also uses the Arduino OTA functionality. So when the ESP8266 successfully connected to your Wifi you will
  * be able to use the Arduino IDE over-the-air functionality.
- *
- *
+ * 
+ * 
  * What you need to configure:
  *  - WiFi SSID + PASS
  *  - MQTT Server (IP(+Username+PW))
  *  - MQTT Topic prefix
- *
+ *      
  * Commands that can be sent through mqtt to the /feed topic.
- *
+ * 
  * There are overall two MQTT topics:
  *    (1) $prefix/feeder-$chipid/state
  *        This topic will hold the current state of the feeder. It will show a number starting from 0 up.
  *        When the feeder is ready it will be 0. When it's currently feeding it will be 1 and up - counting down for
- *        every successfull turn done.
+ *        every successfull turn done. 
  *        There is an safety cut-off for the motor. If the motor is longer active than configured in the MaximumMotorRuntime
  *        variable it will shut-off by itself and set the state to -1.
- *
+ *        
  *    (2) $prefix/feeder-$chipid/feed
  *        This topic acts as the command topic to start / control the feeding process. If you want to start the process
  *        you would send the number of turns you want to happen. So 1 to 5 seems reasonable. The feeder will show the
@@ -32,12 +32,12 @@
  *        You can update the amount any time to shorten / lengthen the process.
  *        On the very first feed request after initial power-up / reboot the feeder will do a calibration run. This is to make
  *        sure that all the wheels are in the right position to work flawlessly.
- *
+ *        
  * examples (for feeder-00F3B839):
- *
+ * 
  * mosquitto_pub -t house/stappenbach/feeder/feeder-00F3B839/feed -m 3
  * -> will feed 3 times
- *
+ * 
  * mosquitto_sub -v -t house/stappenbach/feeder/feeder-00F3B839/state
  * -> will show the current state of the feeder
  */
@@ -53,18 +53,18 @@
 
 
 #ifndef STASSID
-#define STASSID "wifi-ssid"
-#define STAPSK  "wifi-password"
+#define STASSID "yourwifissid"
+#define STAPSK  "yourwifipassword"
 #endif
 
 // mqtt
-const char mqttServer[] = "";
+const char mqttServer[] = "mqtt-broker-ip";
 const int mqttPort = 1883;
 const char mqttUser[] = "yourMQTTuser";
 const char mqttPassword[] = "yourMQTTpassword";
-const char mqtt_topic_prefix[] = "house/feeder";
+const char mqtt_topic_prefix[] = "feeder";
 
-// will show up on topics house/feeder/feeder-$chipid/state and /feed
+// will show up on topics feeder/feeder-$chipid/state and /feed
 // send to topic feed a number of how many feed turns you want it to make
 
 // MQTT
@@ -73,13 +73,11 @@ WiFiClient wificlient;
 char mqtt_node_name[255];
 char mqtt_feed_topic[255];
 char mqtt_feeder_topic[255];
-String rgbstring;
 // Timer: Auxiliary variables
 unsigned long now = millis();
-unsigned long lastMQTTLoopActivation = 0;
 #define MQTTLoopTime 10000
-
-
+unsigned long lastMQTTLoopActivation = 0;
+long MQTTConnectRetriesUntilReboot = 10;
 
 // Counter einbauen der maximale Laufzeit / Fütterzeit und dann sperrt
 int motorPin = D2;
@@ -102,13 +100,18 @@ int toFeedCounter = 0;
 int motorspeed = 0;
 unsigned long LastMotorCommand = 0;
 unsigned long MillisSinceMotorCommand = 0;
-
+bool MotorIsRunning = false;
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
 void setMotor(int speed)
 {
+  if (speed > 0)
+    MotorIsRunning = true;
+  else
+    MotorIsRunning = false;
+  
   Serial.print("Motor-Speed-Control: ");
   Serial.println(speed);
   motorspeed = speed;
@@ -126,9 +129,12 @@ bool isCurrentlyFeeding()
 
 void Feed()
 {
+  char counter[10];
+  sprintf(counter,"%i",toFeedCounter);
+  
   if (bootup_started)
         calibration();
-
+        
   if (toFeedCounter > 0)
   {
     // feedCounter is bigger than 0 - we must start feeding, or we are already feeding...
@@ -149,7 +155,7 @@ void Feed()
             waiting_for_button_to_become_1 = false;
           }
         }
-        // here we are when the button just switched
+        // here we are when the button just switched 
         if (waiting_for_button_to_become_0)
         {
           // we are now waiting for the spinner to rest on the switch, turning it to ...
@@ -159,18 +165,19 @@ void Feed()
             toFeedCounter--;
             if (mqttclient.connected())
             {
-              char counter[10];
-              sprintf(counter,"%i",toFeedCounter);
-              if (mqttclient.connected())
-              {
-                mqttclient.publish(mqtt_feeder_topic,counter);
-              }
+
+                mqttclient.publish(mqtt_feeder_topic,counter);     
+            }
+            else
+            {
+              // we are not connected anymore?
+              Serial.println("MQTT connection error");
             }
 
             setMotor(0);
           }
-
-        }
+         
+        }        
       }
     }
     else
@@ -183,19 +190,32 @@ void Feed()
         calibration(); // call for a calibration -> this will make sure that the buttonstate is correctly at 0 and went through a 0-1-0 cycle
       else
       {
-        setMotor(MaxMotorDutyCycle);       // the motor is not yet spinning, let's get rolling!
+        setMotor(MaxMotorDutyCycle);       // the motor is not yet spinning, let's get rolling!      
         waiting_for_button_to_become_0 = false; // wait...
         waiting_for_button_to_become_1 = true;
       }
     }
   }
-  // nothing to do...
+  else // nothing to do...so check on sending out a life-signal
+  {
+      now = millis();
+  
+      if ( (now - lastMQTTLoopActivation) > MQTTLoopTime)
+      {
+        // send our current state
+        mqttclient.publish(mqtt_feeder_topic,counter);
+        lastMQTTLoopActivation = now;
+      }
+
+  }
+  
+  
 }
 
 void calibration()
 {
   if (!calibrated)
-  {
+  {    
     if (!bootup_started) {
       Serial.println("Calibrating...");
       setMotor(MaxMotorDutyCycle);
@@ -208,7 +228,7 @@ void calibration()
         if (bootup_phase_0_done == false)
         {
           bootup_phase_0_done = true;
-          Serial.println("Calibration Phase #1 done...progressing...");
+          Serial.println("Calibration Phase #1 done...progressing...");        
         }
       }
       // when in end-phase
@@ -229,7 +249,7 @@ void calibration()
   }
 }
 
-void messageReceived(String &topic, String &payload)
+void messageReceived(String &topic, String &payload) 
 {
   Serial.println("--------------> mqtt incoming: " + topic + " - " + payload);
   // check for commands
@@ -242,7 +262,7 @@ void messageReceived(String &topic, String &payload)
     sprintf(counter,"%i",toFeedCounter);
     if (mqttclient.connected())
     {
-      mqttclient.publish(mqtt_feeder_topic,counter);
+      mqttclient.publish(mqtt_feeder_topic,counter);     
     }
   }
 
@@ -254,25 +274,25 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
   int reading = digitalRead(switchPin);
-  buttonState = reading;
-
+  buttonState = reading;  
+  
   Serial.print("Buttonstate: ");
-  Serial.println(buttonState);
+  Serial.println(buttonState);  
   // set-up WiFi and node-name feeder-00F3B839
   snprintf(hostname,255,"feeder-%08X", ESP.getChipId());
   Serial.print("Hostname: ");
   Serial.println(hostname);
-
+  
   snprintf (mqtt_feed_topic, 255, "%s/%s/feed",mqtt_topic_prefix,hostname);
   snprintf (mqtt_feeder_topic, 255, "%s/%s/state",mqtt_topic_prefix,hostname);
 
   Serial.print("MQTT Server: ");
   Serial.println(mqttServer);
-  Serial.println("MQTT Topics: ");
+  Serial.println("MQTT Topics: ");  
   Serial.println(mqtt_feed_topic);
 
 
-
+  
   Serial.println("TurtleFeeder starting up...");
   //Serial.println("Speed 0 to 255");
 
@@ -281,7 +301,7 @@ void setup() {
   pinMode(switchPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(switchPin), triggerSwitch, CHANGE);
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -294,13 +314,19 @@ void setup() {
   mqttclient.onMessage(messageReceived);
 
  int i=0;
-  while (!mqttclient.connect(mqtt_node_name, mqttUser, mqttPassword))
+  while (!mqttclient.connect(mqtt_node_name, mqttUser, mqttPassword)) 
   {
     delay(1000);
-    Serial.print(".");
+    Serial.println("MQTT connect");
     i++;
+
+    if (i >= MQTTConnectRetriesUntilReboot)
+    {
+      Serial.println("Reached too many retries - Restarting...");
+      ESP.restart();
+    }
   }
-  Serial.println(" MQTT connected or timeout");
+  Serial.println(" MQTT connected");
 
   // subscribe to command topic
   //mqttclient.onMessage(messageReceived);
@@ -308,9 +334,9 @@ void setup() {
 
   if (mqttclient.connected())
   {
-    mqttclient.publish(mqtt_feeder_topic,"0");
+    mqttclient.publish(mqtt_feeder_topic,"0");   
   }
-
+  
   // OTA =================================
   // ArduinoOTA.setPort(8266);
   ArduinoOTA.setHostname(hostname);
@@ -358,36 +384,39 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   ArduinoOTA.handle();
-
-  if (!mqttclient.connected()) {
-   int i=0;
-  while (!mqttclient.connect(mqtt_node_name, mqttUser, mqttPassword))
+  
+int i=0;
+  while (!mqttclient.connect(mqtt_node_name, mqttUser, mqttPassword)) 
   {
     delay(1000);
-    Serial.print(".");
+    Serial.println("MQTT connect");
     i++;
+
+    if (i >= MQTTConnectRetriesUntilReboot)
+    {
+      Serial.println("Reached too many retries - Restarting...");
+      ESP.restart();
+    }
   }
-  Serial.println(" MQTT connected or timeout");
-  }
+  //Serial.println(" MQTT connected");
 
   mqttclient.loop();
-
+  
   Feed(); // calls the feed function, won't do anything if the feedCounter is 0
   // check for MaximumMotorRuntime
 
-  if (calibrated)
+  if (MotorIsRunning)
   {
     MillisSinceMotorCommand = millis() - LastMotorCommand;
     if (MillisSinceMotorCommand >= MaximumMotorRuntime)
     {
       if (mqttclient.connected())
       { // report possible error to MQTT
-        mqttclient.publish(mqtt_feeder_topic,"-1");
+        mqttclient.publish(mqtt_feeder_topic,"-1");   
       }
       Serial.println("Maximum Motor Runtime reached: Stopping");
       setMotor(0);
-    }
-
+    }    
   }
 
   if (Serial.available())
@@ -405,10 +434,10 @@ void loop() {
 // gets triggered on switch interrupt...
 ICACHE_RAM_ATTR void triggerSwitch() {
   int reading = digitalRead(switchPin);
-  buttonState = reading;
-
+  buttonState = reading;  
+  
   Serial.print("Buttonstate: ");
-  Serial.println(buttonState);
+  Serial.println(buttonState);  
 }
 // =================================================================================================================
 // =================================================================================================================
